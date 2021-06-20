ae1af50d681c143c8f00c37e2d5b547b52dc26d8
Larry Barrios Jr <github@larrybarriosjr.com>
2021-06-20 15:05:09 +0800

    Add Categories API

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,9 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit"
 import { setupListeners } from "@reduxjs/toolkit/dist/query"
+import { categoriesApiMiddleware, categoriesApiReducer } from "api/categories"
+import { ApiReducerKey } from "constants/enums"
 
 export const store = configureStore({
-  reducer: {},
-  middleware: getDefaultMiddleware => getDefaultMiddleware()
+  reducer: {
+    [ApiReducerKey.CATEGORIES]: categoriesApiReducer
+  },
+  middleware: getDefaultMiddleware => getDefaultMiddleware().concat(categoriesApiMiddleware)
 })
 
 setupListeners(store.dispatch)
