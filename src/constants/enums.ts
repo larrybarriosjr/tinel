886d6017ec421676a1f83cfc5e4bc6c5a4e6a6bb
Larry Barrios Jr <github@larrybarriosjr.com>
2021-06-20 17:30:18 +0800

    Add logo

--- src/constants/enums.ts
+++ src/constants/enums.ts
@@ -4,3 +4,16 @@ export enum ApiReducerKey {
   USERS = "usersApi",
   WORKSHOPS = "workshopsApi"
 }
+
+export enum Color {
+  YELLOW = "var(--yellow)",
+  LIGHT_YELLOW = "var(--light-yellow)",
+  BLUE = "var(--blue)",
+  LIGHT_BLUE = "var(--light-blue)",
+  RED = "var(--red)",
+  BLACK = "var(--black)",
+  WHITE = "var(--white)",
+  LIGHT_GREY = "var(--light-grey)",
+  LIGHTER_GREY = "var(--lighter-grey)",
+  DARKER_GREY = "var(--darker-grey)"
+}
