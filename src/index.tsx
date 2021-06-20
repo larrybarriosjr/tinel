ba21f76af19020856085c5c4accafba888e317ce
Larry Barrios Jr <github@larrybarriosjr.com>
2021-06-20 15:02:18 +0800

    Setup redux toolkit

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,16 @@
 import App from "app/App"
+import { store } from "app/store"
 import React from "react"
 import ReactDOM from "react-dom"
+import { Provider } from "react-redux"
 import "styles/global.scss"
 import * as serviceWorker from "./serviceWorker"
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById("root")
 )
