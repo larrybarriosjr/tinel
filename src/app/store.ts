1f2b751559cb0c9b072be74e0c6dc4943d99de2a
Larry Barrios Jr <github@larrybarriosjr.com>
2021-06-21 23:45:50 +0800

    Setup redux for presentation states

--- src/app/store.ts
+++ src/app/store.ts
@@ -5,13 +5,15 @@ import { ordersApiMiddleware, ordersApiReducer } from "api/orders"
 import { usersApiMiddleware, usersApiReducer } from "api/users"
 import { workshopsApiMiddleware, workshopsApiReducer } from "api/workshops"
 import { ApiReducerKey } from "constants/enums"
+import presentationReducer, { presentationName } from "states/presentation"
 
 export const store = configureStore({
   reducer: {
     [ApiReducerKey.CATEGORIES]: categoriesApiReducer,
     [ApiReducerKey.ORDERS]: ordersApiReducer,
     [ApiReducerKey.USERS]: usersApiReducer,
-    [ApiReducerKey.WORKSHOPS]: workshopsApiReducer
+    [ApiReducerKey.WORKSHOPS]: workshopsApiReducer,
+    [presentationName]: presentationReducer
   },
   middleware: getDefaultMiddleware => {
     return getDefaultMiddleware().concat(
