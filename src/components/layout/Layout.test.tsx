959002aa645d4eb9a7d2c2e404e97dc807f463bf
Larry Barrios Jr <github@larrybarriosjr.com>
2021-06-22 17:07:01 +0800

    Add assertions for cart counter tests

--- src/components/layout/Layout.test.tsx
+++ src/components/layout/Layout.test.tsx
@@ -1,9 +1,26 @@
 import { cleanup, fireEvent, render, RenderResult } from "@testing-library/react"
-import { store } from "app/store"
+import { renderHook } from "@testing-library/react-hooks"
+import { persistor, store } from "app/store"
 import { Routes } from "constants/enums"
+import { useAppDispatch } from "hooks/redux"
 import { Provider } from "react-redux"
 import { MemoryRouter, Route, RouteProps } from "react-router-dom"
+import { PersistGate } from "redux-persist/integration/react"
+import { addToCart, removeFromCart } from "states/cart"
+import { OrderProductsType } from "types/api"
 import Navbar from "./Navbar"
+
+const workshopItem: OrderProductsType = {
+  category: "backend",
+  date: "2020-01-26T13:51:50.417-07:00",
+  desc: "The toughest part is probably to figure out which type of tests to write and how to test some specific logic in your app - but don't give up! Paula will present a few tips she learned along the way that will hopefully make your life easier. In this talk, you will hear about different test types and when to use them, real examples based on PHPUnit and Postman, followed by some tools for checking the test quality",
+  id: 1,
+  imageUrl: "https://pbs.twimg.com/media/EREoip3XsAEPDRp.jpg",
+  price: 350,
+  title: "When you get lost in API testing",
+  userId: 1,
+  quantity: 1
+}
 
 describe("Layout", () => {
   let component: RenderResult
@@ -12,16 +29,18 @@ describe("Layout", () => {
   beforeEach(() => {
     component = render(
       <Provider store={store}>
-        <MemoryRouter>
-          <Navbar />
-          <Route
-            path="*"
-            render={({ location }) => {
-              appLocation = location
-              return null
-            }}
-          />
-        </MemoryRouter>
+        <PersistGate loading={null} persistor={persistor}>
+          <MemoryRouter>
+            <Navbar />
+            <Route
+              path="*"
+              render={({ location }) => {
+                appLocation = location
+                return null
+              }}
+            />
+          </MemoryRouter>
+        </PersistGate>
       </Provider>
     )
   })
@@ -72,7 +91,32 @@ describe("Layout", () => {
     expect(counter).toHaveTextContent(/workshop/i)
   })
 
-  it.todo("changes the item counter depending on workshops added")
+  it("changes the item counter depending on workshops added", () => {
+    const wrapper = ({ children }: { children: React.ReactChild }) => (
+      <Provider store={store}>{children}</Provider>
+    )
+    const { result } = renderHook(() => useAppDispatch(), { wrapper })
+    const dispatch = result.current
+
+    const drawerCounter = component.getByTestId("drawer-cart-counter")
+    const navbarCounter = component.getByTestId("navbar-cart-counter")
+
+    fireEvent.click(component.getByTestId("cart-button"))
+    expect(drawerCounter).toHaveTextContent(/0/i)
+    expect(navbarCounter).toHaveTextContent(/empty/i)
+
+    dispatch(addToCart(workshopItem))
+    expect(drawerCounter).toHaveTextContent(/1/i)
+    expect(navbarCounter).toHaveTextContent(/1/i)
+
+    dispatch(addToCart(workshopItem))
+    expect(drawerCounter).toHaveTextContent(/2/i)
+    expect(navbarCounter).toHaveTextContent(/2/i)
+
+    dispatch(removeFromCart(1))
+    expect(drawerCounter).toHaveTextContent(/0/i)
+    expect(navbarCounter).toHaveTextContent(/empty/i)
+  })
 
   it("renders the close button in the cart drawer", () => {
     fireEvent.click(component.getByTestId("cart-button"))
