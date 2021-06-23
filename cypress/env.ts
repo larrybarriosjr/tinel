0030ad6b3b0c2f4f63ea1c3d9f532db5c87fc9e0
Larry Barrios Jr <github@larrybarriosjr.com>
2021-06-23 20:24:59 +0800

    Setup environment variables for cypress

---
+++ cypress/env.ts
@@ -0,0 +1,12 @@
+// API Base URL
+export const BASE_URL = Cypress.env("BASE_URL")
+
+// API URL Paths
+export const CATEGORIES_URL = Cypress.env("CATEGORIES_URL")
+export const ORDERS_URL = Cypress.env("ORDERS_URL")
+export const USERS_URL = Cypress.env("USERS_URL")
+export const WORKSHOPS_URL = Cypress.env("WORKSHOPS_URL")
+
+// API URL Queries
+export const WORKSHOPS_PAGE = Cypress.env("WORKSHOPS_PAGE")
+export const WORKSHOPS_LIMIT = Cypress.env("WORKSHOPS_LIMIT")
