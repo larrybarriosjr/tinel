011e681f9687da4f33409ecbabdfb5f34fa4035f
Larry Barrios Jr <github@larrybarriosjr.com>
2021-06-25 14:40:29 +0800

    Add title and item count to home page

--- src/components/workshop/WorkshopList.tsx
+++ src/components/workshop/WorkshopList.tsx
@@ -12,10 +12,18 @@ const WorkshopList = ({ items }: WorkshopListProps) => {
   if (!items.length) return null
 
   return (
-    <Flex className={styles.list__container}>
-      {sortByDateDesc(items, "date").map(item => (
-        <WorkshopItem key={item.id} item={item} />
-      ))}
+    <Flex className={styles.list__box}>
+      <div className={styles.list__title_container}>
+        <h2>Workshops</h2>
+        <h6>
+          <span className={styles.list__displayed}>Displayed:</span> {items.length}
+        </h6>
+      </div>
+      <Flex className={styles.list__container}>
+        {sortByDateDesc(items, "date").map(item => (
+          <WorkshopItem key={item.id} item={item} />
+        ))}
+      </Flex>
     </Flex>
   )
 }
